06e0f7db882f86fde439a61cfa73fd1c5d6e8255
Phillip <phillip.besse@gmail.com>
2022-07-14 21:30:59 -0400

    prompt running in node and responses are logged as object

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+function getInfo () {
+    inquirer.prompt([
+        {
+            name: "email",
+            message: "What is your email address?",
+            type: "input",
+        },
+        {
+            name: "gituser",
+            message: "What is your GitHub username?",
+            type: "input",
+        },
+        {
+            name: "title",
+            message: "What is the title of your project?",
+            type: "input",
+        },
+        {
+            name: "description",
+            message: "How would you describe your project?",
+            type: "input",
+        },
+        {
+            name: "install",
+            message: "What are the installation instructions for your project, if any?",
+            type: "input",
+        },
+        {
+            name: "usage",
+            message: "How does one use your project?",
+            type: "input",
+        },
+        {
+            name: "contribution",
+            message: "How can I contribute to your project?",
+            type: "input",
+        },
+        {
+            name: "license",
+            message: "What license would you like to use for your project?",
+            type: "rawlist",
+            choices: ["Apache", "MIT", "GNU"]
+        },
+        {
+            name: "test",
+            message: "How can your project be tested?",
+            type: "input",
+        },
+        {
+            name: "questions",
+            message: "How can one ask questions about your project?",
+            type: "input",
+        },
+])
+    .then((response) => {
+        console.log(response);
+    })
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+// function writeToFile(fileName, data) {}
 
 // Function call to initialize app
-init();
+getInfo();
